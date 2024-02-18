976fd52fed32cf808ebf4a085d992ccb68810f50
abirghaffar <abir.benabdelghaffar@etu.inp-ensat.fr>
2024-02-18 00:34:19 -1100

    first commit

---
+++ CNUM_Auto_Agenda.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 12:30:28 2024
+
+@author: abir
+"""
+import pandas as pd
+from ics import Calendar, Event
+
+
+# Étape 1: Extraction des données Excel
+df = pd.read_excel("C:/Users/abir/Documents/Projet _ Conception numerique/EDT_SIGMA.xlsx",
+                   sheet_name= 'Export CSV')
+df.head ()
+# Convertion le DataFrame en CSV
+df.to_csv('mon_fichier.csv', index=False)
+df.head()
+
+# Créer un nouveau calendrier
+cal = Calendar()
+# Parcourir chaque ligne du DataFrame
+for index, row in df.iterrows():
+    event = Event()
+    event.name = row['Subject']
+    event.begin = row['Start Date'].strftime('%Y-%m-%d') + ' ' + row['Start Time'].strftime('%H:%M:%S')
+    event.end = row['End Date'].strftime('%Y-%m-%d') + ' ' + row['End Time'].strftime('%H:%M:%S')
+    event.location = str(row ['Location'])
+    event.description = row ['Description']
+    cal.events.add(event)
+# Écrire le calendrier dans un fichier .ics
+with open('mon_calendrier.ics', 'w') as my_file:
+    my_file.writelines(cal)
+    
